e884b9edd6c3564b2b2dd3f379cedb5b3ba4d225
Chandram-Dutta <chandramdutta2004@gmail.com>
2024-07-07 17:29:49 +0530

    feat: added new bottom bar

--- VITTY/VITTY/Home/View/HomeView.swift
+++ VITTY/VITTY/Home/View/HomeView.swift
@@ -8,16 +8,34 @@
 import SwiftUI
 
 struct HomeView: View {
+	
+	@State private var selectedPage = 0
+	
 	var body: some View {
-		TabView {
-			TimeTableView(friend: nil)
-				.tabItem {
-					Label("Time Table", systemImage: "calendar.day.timeline.left")
+		ZStack (alignment: .bottom) {
+			switch (selectedPage) {
+			case 0:
+				NavigationStack {
+					ZStack {
+						BackgroundView()
+					}
+					.navigationTitle("Courses")
 				}
-			ConnectPage()
-				.tabItem {
-					Label("Connect", systemImage: "person.2")
-				}
+			case 1:
+				TimeTableView(friend: nil)
+					.tabItem {
+						Label("Schedule", systemImage: "calendar.day.timeline.left")
+					}
+			case 2:
+				ConnectPage()
+					.tabItem {
+						Label("Connect", systemImage: "person.2")
+					}
+			default:
+				Text("Error Lol")
+			}
+			BottomBarView(presentTab: $selectedPage)
+				.padding(.bottom, 24)
 		}
 	}
 }
